644b0a20e63cc603402f29949cd36a486e01a653
Tarun Sunkaraneni <tarun.sunkaraneni@utah.edu>
2018-07-17 16:18:07 -0600

    Solved the problem

--- src/longestPalindromicSubstring.kt
+++ src/longestPalindromicSubstring.kt
@@ -2,20 +2,19 @@ class LongestPalindromicSubstring {
     var s: String = ""
     fun longestPalindromicSubstring(s: String): String {
         var N = s.length
-        val array: Array<BooleanArray> = Array(N, {BooleanArray(N)})
+        val array: Array<BooleanArray> = Array(N+1 ,{index -> BooleanArray(N+1, {index2 -> true})})
         var start = 0
         var len = 1
 
-        for (k in 1 until N) {
-            for (i in 1..N-k) {
-                array[i][i+k] = array[i+1][1+k-1] && array[i-1] == array[i+k-1]
-                if (array[i][i+k] && k+1 > len) {
-                    start = i-1
-                    len = k+1
+        for (i in 1 until N) {
+            for (j in 1..N-i) {
+                array[j][j+i] = array[j+1][j+i-1] && s.substring(j-1,j) == s.substring(j+i-1,j+i)
+                if (array[j][j+i] && i+1 > len) {
+                    start = j-1
+                    len = i+1
                 }
             }
         }
-
         return s.substring(start, start+len)
     }
 }
