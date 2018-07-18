f9c834c58cd7b3deb1a22eedcf0cbb12fea9b025
Tarun Sunkaraneni <tarun.sunkaraneni@utah.edu>
2018-07-18 17:56:28 -0600

    Optimized DP algorithm

--- src/longestPalindromicSubstring.kt
+++ src/longestPalindromicSubstring.kt
@@ -2,24 +2,27 @@ class LongestPalindromicSubstring {
     var s: String = ""
     fun longestPalindromicSubstring(s: String): String {
         var N = s.length
-        val array: Array<BooleanArray> = Array(N+1 ,{index -> BooleanArray(N+1, {index2 -> true})})
+        if (N == 1) return s
+        val array: Array<BooleanArray> = Array(N + 1, { index -> BooleanArray(N + 1, { index2 -> true }) })
         var start = 0
-        var len = 1
+        var maxDist = 0
 
-        for (i in 1 until N) {
-            for (j in 1..N-i) {
-                array[j][j+i] = array[j+1][j+i-1] && s.substring(j-1,j) == s.substring(j+i-1,j+i)
-                if (array[j][j+i] && i+1 > len) {
-                    start = j-1
-                    len = i+1
+        for (dist in 1 until N) {
+            var i = 0
+            while (i + dist < N) {
+                array[i][i + dist] = array[i + 1][i + dist - 1] && s.substring(i, i + 1) == s.substring(i + dist, i + dist + 1)
+                if (array[i][i + dist] && dist > maxDist) {
+                    start = i
+                    maxDist = dist
                 }
+                i++
             }
         }
-        return s.substring(start, start+len)
+        return s.substring(start, start + maxDist+1)
     }
 }
 
-fun main(args: Array<String>){
+fun main(args: Array<String>) {
     var longestPalindrome = LongestPalindromicSubstring()
-    println(longestPalindrome.longestPalindromicSubstring("ababa"))
+    println(longestPalindrome.longestPalindromicSubstring("cbbd"))
 }
